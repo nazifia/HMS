d84906d628ecc938b4aa9d943fdbcc6b648c91dc
nazifia <143083164+nazifia@users.noreply.github.com>
2025-07-05 18:50:38 +0000

    changes

--- retainership/utils.py
+++ retainership/utils.py
@@ -2,4 +2,4 @@ import random
 
 def generate_retainership_reg_number():
     """Generate a unique 10-digit retainership registration number starting with 3."""
-    return int(f"3{random.randint(100000000, 999999999)}")
+    return int(f"3{random.randint(0, 999999999):09d}")
