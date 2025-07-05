6e8f0d592e86d8ec2b30acc6bb387b2b15fe6909
nazifia <143083164+nazifia@users.noreply.github.com>
2025-07-05 22:03:37 +0000

    update

--- retainership/urls.py
+++ retainership/urls.py
@@ -5,6 +5,8 @@ app_name = 'retainership'
 
 urlpatterns = [
     path('patients/', views.retainership_patient_list, name='retainership_patient_list'),
-    path('register-patient/', views.register_patient_for_retainership, name='register_patient_for_retainership'),
+    path('select-patient/', views.select_patient_for_retainership, name='select_patient_for_retainership'),
+    path('register-patient/<int:patient_id>/', views.register_patient_for_retainership, name='register_patient_for_retainership'),
+    path('register-patient/', views.select_patient_for_retainership, name='register_patient_no_id'),
     path('register-independent/', views.register_independent_retainership_patient, name='register_independent_retainership_patient'),
 ]
